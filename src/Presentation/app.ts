bd8d5dae56ed89385e7c7be6dd2e6aeef85a05d1
Draken-101 <leonardofavion@gmail.com>
2024-07-07 19:53:28 -0600

    rework

--- src/Presentation/app.ts
+++ src/Presentation/app.ts
@@ -1,26 +1,32 @@
 import { WebSocket, WebSocketServer } from 'ws';
 import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import * as https from 'https';
 import { custom, customText } from '../Services/customSignale';
-
 dotenv.config();
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
-interface IWebSocket extends WebSocket{
-    username:string;
+interface IWebSocket extends WebSocket {
+    username: string;
 }
 
-const wss = new WebSocketServer({ port: PORT });
+const server = https.createServer({
+    cert: fs.readFileSync('./src//certificates/cert.pem'),
+    key: fs.readFileSync('./src/certificates/key.pem')
+})
+
+const wss = new WebSocketServer({ server });
 
 wss.on('connection', (ws: IWebSocket) => {
     ws.on('message', (message) => {
-        const user = JSON.parse(message.toString())
+        const user = JSON.parse(message.toString()) 
         ws.username = user.nameUser;
 
         custom.NewUser(
             customText.bold + customText.colors.cyan + ' | ' + customText.end +
-            customText.colors.magenta + 'Usuario conectado: ' + 
-            customText.bold + customText.colors.blanco + ws.username  + customText.end +
+            customText.colors.magenta + 'Usuario conectado: ' +
+            customText.bold + customText.colors.blanco + ws.username + customText.end +
             customText.bold + customText.colors.cyan + ' | ' + customText.end
         );
 
@@ -34,16 +40,20 @@ wss.on('connection', (ws: IWebSocket) => {
     ws.on('close', () => {
         custom.Close(
             customText.bold + customText.colors.cyan + '| ' + customText.end +
-            customText.colors.magenta + 'Usuario desconectado: ' + 
-            customText.bold + customText.colors.blanco + ws.username  + customText.end +
+            customText.colors.magenta + 'Usuario desconectado: ' +
+            customText.bold + customText.colors.blanco + ws.username + customText.end +
             customText.bold + customText.colors.cyan + '| ' + customText.end
         );
     });
 });
-console.clear();
-custom.Success(
-    customText.bold + customText.colors.cyan + '| ' + customText.end +
-    customText.colors.magenta + 'Servidor corriendose en el puerto: ' + 
-    customText.bold + customText.colors.blanco + PORT +  customText.end +
-    customText.bold + customText.colors.cyan + ' |' + customText.end
-)
+
+
+server.listen(PORT, () => {
+    console.clear();
+    custom.Success(
+        customText.bold + customText.colors.cyan + '| ' + customText.end +
+        customText.colors.magenta + 'Servidor corriendose en el puerto: ' +
+        customText.bold + customText.colors.blanco + PORT + customText.end +
+        customText.bold + customText.colors.cyan + ' |' + customText.end
+    )
+})
